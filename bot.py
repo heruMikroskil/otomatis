d7fce0996c238bb3b68e2535776427bcbe1766fe
heruMikroskil <heru.kurniawan@mikroskil.ac.id>
2026-08-27 16:19:53 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -39,21 +39,21 @@ async def main():
         # 1. Buka halaman website
         print("Membuka halaman website...")
         await page.goto("https://virtual-expo.lkpp.go.id/visitor/register")
-        await page.screenshot(path="01_halaman_awal.png")
+        #await page.screenshot(path="01_halaman_awal.png")
         # 2. Isi data berdasarkan ID elemen (#id_elemen)
         # Ganti 'username_input' dan 'password_input' dengan ID asli di website
         print("Mengisi formulir...")
         # await page.fill("#Nama", "user_anda")
         # await page.fill("#Email", "email_anda")
-        await page.fill("#profile_name", "Player008")
-        await page.fill("#profile_email", "o.player008@gmail.com")
+        await page.fill("#profile_name", "Player009")
+        await page.fill("#profile_email", "o.player009@gmail.com")
         await page.fill("#profile_company_name", "Indonesia")
         await page.fill("#profile_occupation", "Boss")
         await page.fill("#profile_phone_number", "082288997788")
         await page.fill("#profile_password", "Admin123")
         await page.fill("#profile_password_confirmation", "Admin123")
         await page.check("input.form-check-input")
-        await page.screenshot(path="02_halaman_awal.png")
+        #await page.screenshot(path="02_halaman_awal.png")
 
         # 3. Klik tombol/bagian tertentu (berdasarkan ID atau teks)
         # Contoh klik elemen dengan ID '#submit-btn'
@@ -62,20 +62,28 @@ async def main():
 
         # Tunggu proses pemuatan setelah klik (opsional)
         await page.wait_for_timeout(5000)
-        await page.screenshot(path="03_halaman_awal.png")
+        #await page.screenshot(path="03.png")
         await asyncio.sleep(3) # Tunggu elemen/canvas termuat sempurna
 
         # Panggil fungsi klik berdasarkan teks di dalam gambar/canvas
         #await click_text_on_image(page, "Lewati")
         await page.mouse.click(360, 1008)
         await page.wait_for_timeout(2000)
-        await page.screenshot(path="04_halaman_awal.png")
+        await page.screenshot(path="04.png")
         print("Lewati Selesai")
         #Close banner
         await page.mouse.click(593, 530)
         await page.wait_for_timeout(2000)
-        await page.screenshot(path="05_halaman_awal.png")
+        await page.screenshot(path="05.png")
         print("Close banner selesai")
+        #Masukk Hall
+        await page.mouse.click(277, 654)
+        await page.wait_for_timeout(2000)
+        await page.screenshot(path="06.png")
+        print("Masuk Hall selesai")
+
+
+        
         await browser.close()
 
 if __name__ == "__main__":
